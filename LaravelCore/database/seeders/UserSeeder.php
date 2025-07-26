8c7076917e3ef468f38c83978f3e8b9d016af1e8
CaoHuy <148456721+CaoHuy@users.noreply.github.com>
2025-07-26 10:09:49 +0700

    Cập nhật seeder chi nhánh và branch

--- LaravelCore/database/seeders/UserSeeder.php
+++ LaravelCore/database/seeders/UserSeeder.php
@@ -80,8 +80,7 @@ class UserSeeder extends Seeder
 
         DB::statement("
             INSERT INTO `user_warehouse` (`user_id`, `warehouse_id`) VALUES
-            (1, 4), (1, 5), (1, 6), (1, 7),
-            (2, 4), (2, 5), (2, 6), (2, 7);
+            (1, 1), (2, 1), (3, 1);
         ");
     }
 }
--- LaravelCore/database/seeders/WarehouseSeeder.php
+++ LaravelCore/database/seeders/WarehouseSeeder.php
@@ -16,14 +16,9 @@ class WarehouseSeeder extends Seeder
     public function run()
     {
         $warehouses = [
-            [1, 1, NULL, 'Kho vật tư y tế', 'Cái Răng, Cần Thơ', NULL, 0, '2024-08-31 02:41:55', '2024-06-09 17:09:32', '2024-08-31 02:41:55'],
-            [1, 2, NULL, 'Kho thuốc', NULL, NULL, 0, '2024-08-31 02:41:50', '2024-06-09 17:09:32', '2024-08-31 02:41:50'],
-            [1, 3, NULL, 'Kho đồ ăn & đồ dùng', NULL, NULL, 0, '2024-08-31 02:41:45', '2024-06-09 17:09:32', '2024-08-31 02:41:45'],
-            [1, 4, 1, 'Kho nội bộ 586', 'Cái Răng, Cần Thơ', NULL, 1, NULL, '2024-06-09 17:09:32', '2024-08-31 19:54:55'],
-            [1, 5, 1, 'Kho bán 586', 'Cái Răng, Cần Thơ', NULL, 2, NULL, '2024-06-09 17:09:32', '2024-09-11 05:09:37'],
-            [1, 6, 2, 'Kho bán THN', 'Ninh Kiều, Cần Thơ', NULL, 2, NULL, '2024-06-09 17:09:32', '2024-09-11 05:09:27'],
-            [1, 7, 2, 'Kho nội bộ THN', 'Ninh Kiều, Cần Thơ', NULL, 1, NULL, '2024-06-09 17:09:32', '2024-08-31 19:54:33'],
-            [2, 8, 3, 'Kho Mậu Thân', 'Ninh Kiều, Cần Thơ', NULL, 1, NULL, '2024-06-10 17:09:32', '2024-06-10 19:54:33'],
+            [1, 1, 1, 'Kho Nguyễn Văn Linh', 'Ninh Kiều, Cần Thơ', NULL, 2, NULL, '2024-06-09 17:09:32', '2024-08-31 19:54:55'],
+            [2, 2, 2, 'Kho Trần Hoàng Na', 'Ninh Kiều, Cần Thơ', NULL, 2, NULL, '2024-06-09 17:09:32', '2024-09-11 05:09:37'],
+            [3, 3, 3, 'Kho Nguyễn Văn Cừ', 'Ninh Kiều, Cần Thơ', NULL, 2, NULL, '2024-06-09 17:09:32', '2024-09-11 05:09:27'],
         ];
 
         foreach ($warehouses as $warehouse) {
